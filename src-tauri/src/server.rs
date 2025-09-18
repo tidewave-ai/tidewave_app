e234f839b0c90916e4a3564c5073ef9de0b2d776
Steffen Deusch <steffen@deusch.me>
2025-09-18 12:15:22 +0200

    Update src-tauri/src/server.rs
    Co-authored-by: Jonatan Kłosko <jonatanklosko@gmail.com>

--- src-tauri/src/server.rs
+++ src-tauri/src/server.rs
@@ -114,6 +114,7 @@ async fn root() -> Html<String> {
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="tidewave:source" content="cli" />
     <script type="module" src="{}/tc/tc.js"></script>
   </head>
   <body></body>
